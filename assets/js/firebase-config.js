8a510daf5e7969ddc75ee67f6c43a4a4b3079409
koariy <infinityseries2025@gmail.com>
2026-07-03 14:15:19 +0300

    Update firebase-config.js

--- assets/js/firebase-config.js
+++ assets/js/firebase-config.js
@@ -6,13 +6,7 @@
    коде сайта, реальная защита данных настраивается правилами
    Firestore (см. FIREBASE_SETUP.md в корне проекта).
 ========================================================= */
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyBUzCjDqDmuaKLQY3Q5zhYhgfuOqnPgdak",
